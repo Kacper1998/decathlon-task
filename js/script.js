e19c53c03389ffb5ecb57b166b5186f0c695bd4f
Kacper Radzik <KacperRadzik@op.pl>
2023-09-22 11:11:46 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,39 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let products = document.querySelector('.products');
-    async function fetchProducts(url) {
-        try {
-            let data = await fetch(url);
-            let response = await data.json();
-
-            for (let i = 0; i < 3; i++) {
-                let description = response[i].description;
-                let title = response[i].title;
-                products.innerHTML += `
-       <div class="product">
-           <img src="${response[i].images[1]}" alt="${
-          response[i].category.name
-        }" class="product-img">
-           <div class="product-content">
-           <h2 class="product-title">${
-             title.length > 34 ? title.substring(0, 34).concat(' ...') : title
-           }</h2>
-     
-           <div class="product-price-container">
-               <h3 class="product-price">$${response[i].price}</h3>
-               <a href="#!" data-productId="${
-                 response[i].id
-               }" class="add-to-cart"><ion-icon name="cart-outline"></ion-icon></a>
-           </div>
-           </div>
-          
-       </div>
-       `;
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    }
-    fetchProducts('https://api.escuelajs.co/api/v1/products');
+fetch("https://fakestoreapi.com/products?limit=3")
+.then(response => response.json())
+.then((data) => {
+    let data1 = "";
+    data.map((values) => {
+        data1+= `
+        <div class="card">
+            <div class="img">
+                <img src=${values.image} id="poster">
+            </div>
+            <h2 id="title">${values.title}</h2>
+            <div class="price">
+                <h4 id="price">$${values.price}</h4>
+                <h5 id="rating">${values.rating.rate}</h5>
+            </div>
+        </div>
+        `
+        document.querySelector('.grid-item').innerHTML=data1;
+    })
+})
+.catch(err => {
+	console.error(err);
 });
 
 $(document).ready(function() {
@@ -93,3 +79,23 @@ fetch('https://fakestoreapi.com/products')
     displayCocktail(data)
   })
   .catch((error) => console.error("FETCH ERROR:", error));
+
+  $( document ).ready(function() {
+
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".hamburger" ).hide();
+    $( ".cross" ).show();
+    });
+    });
+    
+    $( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".cross" ).hide();
+    $( ".hamburger" ).show();
+    });
+    });
+    
+    });
